dba88a7e92b99022ee8c6170843d96054cc91fcc
Toshiaki Maki <makingx@gmail.com>
2014-08-13 02:14:40 +0900

    add actuator

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/image/**", "/api/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/image/**", "/api/**", "/management/**");
     }
 
     @Override
